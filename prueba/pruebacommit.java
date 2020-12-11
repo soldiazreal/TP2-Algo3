3f2173757ecb8aa6efc6ef013679e842ba9ed92e
marcosldvargas <mlvargas@gmail.com>
2020-12-11 10:29:16 -0300

    mi cambio importante

--- prueba/pruebacommit.java
+++ prueba/pruebacommit.java
@@ -3,4 +3,10 @@ public class pruebacommit {
     public static void main(String[] args) {
         System.out.println("Pruebo mi primer commit");
     }
+
+    public void imprimir(){
+        System.out.println("Pruebo mi primer commit -marcola");
+    }
 }
+
+
