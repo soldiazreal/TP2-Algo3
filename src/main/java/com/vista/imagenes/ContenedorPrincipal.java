4d4520e6aedc39c444e86bce82844e53b46973b1
marcosldvargas <mlvargas@fi.uba.ar>
2021-02-15 14:21:45 -0300

    probando vista

--- src/main/java/com/vista/imagenes/ContenedorPrincipal.java
+++ src/main/java/com/vista/imagenes/ContenedorPrincipal.java
@@ -2,6 +2,7 @@ package com.vista.imagenes;
 
 import com.tablero.Tablero;
 import com.vista.eventos.BotonCrearPersonalizadoHandler;
+import com.vista.eventos.BotonReiniciarHandler;
 import com.vista.eventos.BotonReproducirHandler;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
@@ -17,38 +18,42 @@ public class ContenedorPrincipal extends BorderPane {
     Canvas canvasCentral;
     BarraDeMenu menuBar;
     VBox contenedorCentral;
+    Tablero tablero;
 
     public ContenedorPrincipal (Stage stage, Tablero tablero) {
+        this.tablero = tablero;
         this.setMenu(stage);
         ContenedorPersonalizado contenedorPersonalizado = new ContenedorPersonalizado();
         Scene escenaPersonalizado = new Scene(contenedorPersonalizado, 640, 480);
-        ContenedorSimulacion contenedorSimulacion = new ContenedorSimulacion();
+        ContenedorSimulacion contenedorSimulacion = new ContenedorSimulacion(stage, tablero, new Scene(new ContenedorPrincipal(stage, tablero), 640, 480));
         Scene escenaSimulacion = new Scene(contenedorSimulacion, 640, 480);
         this.setBotonera(stage, escenaPersonalizado, escenaSimulacion);
 
     }
 
     public void setBotonera(Stage stage, Scene escenaPersonalizado, Scene escenaSimulacion) {
+
         Button botonCrearPersonalizado = new Button();
         botonCrearPersonalizado.setText("Crear bloque personalizado");
         BotonCrearPersonalizadoHandler crearPersonalizadoHandler = new BotonCrearPersonalizadoHandler(stage, escenaPersonalizado);
         botonCrearPersonalizado.setOnAction(crearPersonalizadoHandler);
 
         Button botonReproducir = new Button();
-        botonReproducir.setText("Reproducir simulación!");
+        botonReproducir.setText("Reproducir");
         BotonReproducirHandler botonReproducirHandler = new BotonReproducirHandler(stage, escenaSimulacion);
         botonReproducir.setOnAction(botonReproducirHandler);
 
         Button botonReiniciar = new Button();
-        botonReiniciar.setText("Reiniciar programa");
-
+        botonReiniciar.setText("Reiniciar");
+        BotonReiniciarHandler botonReiniciarHandler = new BotonReiniciarHandler(tablero);
+        botonReiniciar.setOnAction(botonReiniciarHandler);
         botonReiniciar.setStyle("-fx-border-color: #ff0000; -fx-border-width: 5px;");
        // botonReiniciar.setStyle("-fx-background-color: #00ff00");
        // botonReiniciar.setStyle("-fx-font-size: 2em; ");
         botonReiniciar.setStyle("-fx-text-fill: #ff0000");
 
         HBox contenedorHorizontal = new HBox(botonCrearPersonalizado, botonReproducir, botonReiniciar);
-        contenedorHorizontal.setSpacing(300);
+        contenedorHorizontal.setSpacing(100);
         contenedorHorizontal.setPadding(new Insets(15));
 
         this.setBottom(contenedorHorizontal);
--- src/main/java/com/vista/imagenes/ContenedorSimulacion.java
+++ src/main/java/com/vista/imagenes/ContenedorSimulacion.java
@@ -1,6 +1,102 @@
 package com.vista.imagenes;
 
+import com.tablero.Tablero;
+import com.vista.eventos.BotonCrearPersonalizadoHandler;
+import com.vista.eventos.BotonReproducirHandler;
+import com.vista.eventos.BotonVolverHandler;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Line;
+import javafx.stage.Stage;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ContenedorSimulacion extends BorderPane {
+
+    Scene escenaAnterior;
+    Tablero tablero;
+
+    public ContenedorSimulacion (Stage stage, Tablero tablero, Scene escenaAnterior) {
+        this.tablero = tablero;
+        this.setDibujo(stage, tablero);
+        this.escenaAnterior = escenaAnterior;
+    }
+
+    public void setBotonera (Stage stage, Tablero tablero) {
+
+        Button botonParar = new Button();
+        botonParar.setText("Parar");
+        BotonReproducirHandler botonReproducirHandler = new BotonReproducirHandler(stage, new Scene(new ContenedorSimulacion(stage, tablero, escenaAnterior)));
+        botonParar.setOnAction(botonReproducirHandler);
+
+        Button botonVolver = new Button();
+        botonVolver.setText("Volver");
+        BotonVolverHandler botonVolverHandler = new BotonVolverHandler();
+        botonVolver.setOnAction(botonVolverHandler);
+
+        HBox contenedorHorizontal = new HBox(botonVolver, botonParar);
+        contenedorHorizontal.setSpacing(100);
+        contenedorHorizontal.setPadding(new Insets(15));
+
+        this.setBottom(contenedorHorizontal);
+
+    }
+
+    public void setDibujo (Stage stage, Tablero tablero) {
+        Pane seccionDibujado = new Pane();
+        //PARTE DE LINEAS
+        List<Line> lines = new ArrayList<>();
+
+        for(int i = 5; i <= 405; i+= 100) {
+            for (int j = 5; j <= 205; j += 100) {
+                Circle circle = new Circle();
+                circle.setCenterX(i);
+                circle.setCenterY(j);
+                circle.setRadius(3);
+                seccionDibujado.getChildren().add(circle);
+            }
+        }
+
+        for(int i = 5; i < 405; i+= 100) {
+            for(int j = 5; j <= 205; j+= 100) {
+                Line line = new Line();
+                line.setStartX(i);
+                line.setStartY(j);
+                line.setEndX(i + 100);
+                line.setEndY(j );
+                line.setVisible(true);
+                lines.add(line);
+                seccionDibujado.getChildren().add(line);
+            }
+        }
+        for(int i = 5; i <= 405; i+= 100) {
+            for(int j = 5; j < 205; j+= 100) {
+                Line line = new Line();
+                line.setStartX(i);
+                line.setStartY(j);
+                line.setEndX(i );
+                line.setEndY(j + 100);
+                line.setVisible(true);
+                lines.add(line);
+                seccionDibujado.getChildren().add(line);
+            }
+        }
+
+        //la goma de borrar lineas magica
+        for(int i = 0; i < 0; i++){
+            lines.get(i).setVisible(false);
+        }
+        //Termina creacion de lineas
+
+        seccionDibujado.setMaxSize(410, 210); //SECCION DE LAS LINEAS
+        VBox escenaSimulacion = new VBox(seccionDibujado);
+        this.setCenter(escenaSimulacion);
+    }
 }
