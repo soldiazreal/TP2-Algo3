36f0acf387b648f10d9bbc742bff86138a50e11a
lucas <lucasd393939@gmail.com>
2021-02-24 21:04:11 -0300

    Agregando drag y drop

--- src/main/java/com/vista/imagenes/ContenedorPrincipal.java
+++ src/main/java/com/vista/imagenes/ContenedorPrincipal.java
@@ -14,9 +14,19 @@ import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.layout.*;
 import javafx.stage.Stage;
+import javafx.scene.input.*;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.EventHandler;
+import javafx.scene.text.Font;
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class ContenedorPrincipal extends BorderPane {
 
+
+    static final DataFormat STRING_LIST = new DataFormat("StringList");
     ListView<String> bloquesDisponibles;
     ListView<String> bloquesAEjecutar;
     BarraDeMenu menuBar;
@@ -27,9 +37,230 @@ public class ContenedorPrincipal extends BorderPane {
         this.tablero = tablero;
         this.setMenu(stage);
         this.setBotonera(stage);
+        this.setAlgoritmo(stage);
         Personaje personaje = tablero.getPersonaje();
         this.setReproductor(personaje, stage);
     }
+
+    public void setAlgoritmo(Stage stage) {
+
+        this.bloquesDisponibles = new ListView<>();
+        this.bloquesAEjecutar = new ListView<>();
+        bloquesDisponibles.setStyle("-fx-border-color: #bb4c14;" + "-fx-border-width: 4");
+        bloquesAEjecutar.setStyle("-fx-border-color: #bb4c14;" + "-fx-border-width: 4");
+
+        // Creacion del texto para cada zona
+        Label seccionBloquesLbl = new Label("Sección Bloques ");
+        seccionBloquesLbl.setFont(new Font("Tahoma", 19));
+        seccionBloquesLbl.setStyle("-fx-text-fill: #bb4c14");
+        Label seccionAlgoritmosLbl = new Label("  Sección Algoritmo ");
+        seccionAlgoritmosLbl.setFont(new Font("Tahoma", 19));
+        seccionAlgoritmosLbl.setStyle("-fx-text-fill: #bb4c14");
+        Label explicacionLbl = new Label("Arrastra de Sección Bloques a Sección Algoritmo para seleccionar qué se ejecuta");
+        explicacionLbl.setStyle("-fx-text-fill: #bb4c14");
+
+        // Seleccion de como se ven las areas
+        bloquesDisponibles.setPrefSize(200, 350);
+        bloquesAEjecutar.setPrefSize(200, 350);
+
+        // Agregando los bloques a la Source List
+        bloquesDisponibles.getItems().addAll(this.getBloqueList());
+
+        // Permitiendo multiple seleccion en listas
+        bloquesDisponibles.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+        bloquesAEjecutar.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+
+        // Agregandoe el evento del mouse y los handlers para los disponibles
+        bloquesDisponibles.setOnDragDetected(new EventHandler<MouseEvent>()
+        {
+            public void handle(MouseEvent event)
+            {
+                dragDetected(event, bloquesDisponibles);
+            }
+        });
+
+        bloquesDisponibles.setOnDragOver(new EventHandler <DragEvent>()
+        {
+            public void handle(DragEvent event)
+            {
+                dragOver(event, bloquesDisponibles);
+            }
+        });
+
+        // Agregar mouse para los event handlers para el objetivo
+        bloquesAEjecutar.setOnDragDetected(new EventHandler <MouseEvent>()
+        {
+            public void handle(MouseEvent event)
+            {
+                dragDetected(event, bloquesAEjecutar);
+            }
+        });
+
+        bloquesAEjecutar.setOnDragOver(new EventHandler <DragEvent>()
+        {
+            public void handle(DragEvent event)
+            {
+                dragOver(event, bloquesAEjecutar);
+            }
+        });
+
+        bloquesAEjecutar.setOnDragDropped(new EventHandler <DragEvent>()
+        {
+            public void handle(DragEvent event)
+            {
+                dragDropped(event, bloquesAEjecutar);
+            }
+        });
+
+        bloquesAEjecutar.setOnDragDone(new EventHandler <DragEvent>()
+        {
+            public void handle(DragEvent event)
+            {
+                dragDone(event, bloquesAEjecutar);
+                removeSelectedBloque(bloquesAEjecutar);
+            }
+        });
+
+        HBox textos = new HBox();
+        textos.getChildren().addAll(seccionBloquesLbl, seccionAlgoritmosLbl);
+        textos.setSpacing(80);
+        textos.setPadding(new Insets(10));
+        textos.setAlignment(Pos.CENTER);
+
+        HBox listas = new HBox(bloquesDisponibles, bloquesAEjecutar);
+        listas.setSpacing(50);
+        listas.setAlignment(Pos.CENTER);
+
+        HBox explicacion =  new HBox(explicacionLbl);
+        explicacion.setAlignment(Pos.CENTER);
+
+        VBox general = new VBox(textos, listas, explicacion);
+        general.setSpacing(22);
+        general.setPadding(new Insets(10));
+        general.setAlignment(Pos.CENTER);
+
+        Image imagen = new Image("file:src/main/java/com/vista/imagenes/f7.jpg");
+        BackgroundImage imagenDeFondo = new BackgroundImage(imagen, BackgroundRepeat.REPEAT, BackgroundRepeat.REPEAT, BackgroundPosition.DEFAULT, BackgroundSize.DEFAULT);
+        general.setBackground(new Background((imagenDeFondo)));
+        general.setStyle(
+                "-fx-border-style: solid inside;" +
+                        "-fx-border-width: 4;" +
+                        "-fx-border-color: #241802;");
+
+        this.setCenter(general);
+    }
+
+    private ObservableList<String> getBloqueList()
+    {
+        ObservableList<String> list = FXCollections.<String>observableArrayList();
+
+        String moverDerecha = "Derecha";
+        String moverIzquierda = "Izquierda";
+        String moverArriba = "Arriba";
+        String moverAbajo = "Abajo";
+
+        list.addAll(moverDerecha,moverIzquierda,moverArriba,moverAbajo);
+
+        return list;
+    }
+
+    private void dragDetected(MouseEvent event, ListView<String> listView)
+    {
+        // Estando seguro de que un objeto fue seleccionado
+        int selectedCount = listView.getSelectionModel().getSelectedIndices().size();
+
+        if (selectedCount == 0)
+        {
+            event.consume();
+            return;
+        }
+
+        // Iniciando el drag y drop
+        Dragboard dragboard = listView.startDragAndDrop(TransferMode.COPY);
+
+        // Poniendo los items seleccionados en la dragboard
+        ArrayList<String> selectedItems = this.getSelectedBloque(listView);
+
+        ClipboardContent content = new ClipboardContent();
+        content.put(STRING_LIST, selectedItems);
+
+        dragboard.setContent(content);
+        event.consume();
+    }
+
+    private void dragOver(DragEvent event, ListView<String> listView)
+    {
+        // Si la drag board tiene una ITEM_LIST y no esta siendo arrastrada
+        // sobre si misma, aceptamos el MOVE transfer mode
+        Dragboard dragboard = event.getDragboard();
+
+        if (event.getGestureSource() != listView && dragboard.hasContent(STRING_LIST))
+        {
+            event.acceptTransferModes(TransferMode.COPY_OR_MOVE);
+        }
+
+        event.consume();
+    }
+
+    @SuppressWarnings("unchecked")
+    private void dragDropped(DragEvent event, ListView<String> listView)
+    {
+        boolean dragCompleted = false;
+
+        // Transfiriendo los datos
+        Dragboard dragboard = event.getDragboard();
+
+        if(dragboard.hasContent(STRING_LIST))
+        {
+            ArrayList<String> list = (ArrayList<String>)dragboard.getContent(STRING_LIST);
+            listView.getItems().addAll(list);
+            // Transferencia de datos exitosa
+            dragCompleted = true;
+        }
+
+        // Transferencia de datos no es exitosa
+        event.setDropCompleted(dragCompleted);
+        event.consume();
+    }
+
+    private void dragDone(DragEvent event, ListView<String> listView)
+    {
+        // Esto habria que sacarlo estas removiendo el item no queremos hacer eso
+        TransferMode tm = event.getTransferMode();
+
+        if (tm == TransferMode.MOVE)
+        {
+            removeSelectedBloque(listView);
+        }
+
+        event.consume();
+    }
+
+    private ArrayList<String> getSelectedBloque(ListView<String> listView)
+    {
+        // Devolviendo la lista de Bloques seleccionados en una ArrayList, asi es
+        // serializable y se pueda guardar en una Dragboard.
+        ArrayList<String> list = new ArrayList<String>(listView.getSelectionModel().getSelectedItems());
+
+        return list;
+    }
+
+    private void removeSelectedBloque(ListView<String> listView)
+    {
+        // Metiendo todos los Bloques en una lista separada para no tener el problema de lista compartida
+        List<String> selectedList = new ArrayList<>();
+
+        for(String string : listView.getSelectionModel().getSelectedItems())
+        {
+            selectedList.add(string);
+        }
+
+        // Limpiando la seleccion
+        listView.getSelectionModel().clearSelection();
+        // Removiendo items de la lista seleccionada
+        listView.getItems().removeAll(selectedList);
+    }
+
 
     public void setBotonera(Stage stage) {
 
