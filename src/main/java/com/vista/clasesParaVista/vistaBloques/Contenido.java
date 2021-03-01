56f56e8565b74e6e794d9791f77a98e138ceede9
lucas <lucasd393939@gmail.com>
2021-03-01 18:46:34 -0300

    Vistas de bloques

---
+++ src/main/java/com/vista/clasesParaVista/vistaBloques/Contenido.java
@@ -0,0 +1,23 @@
+package com.vista.clasesParaVista.vistaBloques;
+
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.StackPane;
+
+public class Contenido extends StackPane {
+    ImageView imagenes;
+
+    public Contenido(ImageView imagen){
+        this.getChildren().add(imagen);
+        this.imagenes = imagen;
+    }
+
+    public ImageView getImage(){
+        return this.imagenes;
+    }
+
+    public void agregarNombre(Label nombre){
+        this.getChildren().add(nombre);
+    }
+}
