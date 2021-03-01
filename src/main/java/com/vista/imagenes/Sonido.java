0fcf69b71b3d49eda53859999062663f94704dc6
soldiazreal <soldiazreal@gmail.com>
2021-03-01 03:08:28 -0300

    agrego sonido para bienvenida

---
+++ src/main/java/com/vista/imagenes/Sonido.java
@@ -0,0 +1,26 @@
+package com.vista.imagenes;
+
+import java.io.File;
+//import javafx.scene.media.Media;
+//import javafx.scene.media.MediaPlayer;
+
+public class Sonido {
+
+    public void ReproducirSonido(){
+
+        com.sun.javafx.application.PlatformImpl.startup(()->{});
+
+        final String compuertaAmong = "among-us-2-3d.jpg";
+
+        File sonido = new File(compuertaAmong);
+
+        /*Media audio = new Media(sonido.toURI().toString());
+
+        MediaPlayer reproductor = new MediaPlayer(audio);
+
+        reproductor.play();
+
+    */
+
+    }
+}
